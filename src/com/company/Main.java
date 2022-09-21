c23f2280e4d898999f316bc843a9aec7d8d9973e
Esteban Saez <e.saez07@ufromail.cl>
2022-09-20 23:29:59 -0300

    COMMIT1

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cadena= "la tEle letaL";
+        cadena= cadena.replace(" ", "");
+        cadena= cadena.toLowerCase();
+        //System.out.println("la cadena es: "+cadena);
+        boolean esPalindromo = true;
+        for(int i=0;i<cadena.length()/2;i++){
+            if(cadena.charAt(i)!=cadena.charAt(cadena.length()-1-i)){
+                esPalindromo = false;
+                break;
+            }
+        }
+        if(esPalindromo){
+            System.out.println("La palabra es palindromo");
+        }else{
+            System.out.println("La palabra no es palindromo");
+        }
+    }
+
+}
